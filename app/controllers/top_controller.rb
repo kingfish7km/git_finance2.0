f13baafca9aa1c4a1ee1fb28520d5a928f6f4c76
EC2 Default User <ec2-user@ip-172-31-32-173.ap-northeast-1.compute.internal>
2020-10-03 01:47:49 +0000

    contents、about us、contact usのルーティングおよびviewファイルを作成

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,7 +3,25 @@ class TopController < ApplicationController
  def index
  end
 
+ def show_us
+ end
+ 
+ def show_contact
+ end
+
  def show
+ end
+ 
+ def show_0
+ end
+
+ def show_1
+ end
+
+ def show_2
+ end
+
+ def show_3
  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  root "top#index"
+ get 'about' => 'top#show_us'
+ get 'contact' => 'top#show_contact'
  get 'contents' => 'top#show'
+ get 'contents/0' => 'top#show_0'
+ get 'contents/1' => 'top#show_1'
+ get 'contents/2' => 'top#show_2'
+ get 'contents/3' => 'top#show_3'
 
 end
