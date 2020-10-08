26d23042239fb726886b0a51d1e84e2b653a184a
EC2 Default User <ec2-user@ip-172-31-32-173.ap-northeast-1.compute.internal>
2020-10-08 12:51:02 +0000

    routes.rbにpost 'questions'をルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
  get 'contents' => 'top#show'
  get 'contents/0' => 'top#show_0'
  get 'contents/0' => 'questions#new'
+ post 'questions' => 'questions#create'
  get 'contents/1' => 'top#show_1'
  get 'contents/2' => 'top#show_2'
  get 'contents/3' => 'top#show_3'
