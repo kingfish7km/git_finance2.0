683608e4b489c7c9e43edae0e6a804177117f532
EC2 Default User <ec2-user@ip-172-31-32-173.ap-northeast-1.compute.internal>
2020-10-10 00:46:23 +0000

    question_controllerにQuestion.createを記述

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,13 @@
 class QuestionsController < ApplicationController
   
-  
-  
   def new
   end
   
   def create_0
-    Questions.create()
+    Questions.create(chapter_number: 0, text: question_params[:text], user_id: current_user.id)
   end
+  
+  
   
   
   
