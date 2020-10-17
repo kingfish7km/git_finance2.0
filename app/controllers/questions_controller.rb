9902233074bbb29b59184571ffc8013db560964b
EC2 Default User <ec2-user@ip-172-31-32-173.ap-northeast-1.compute.internal>
2020-10-17 02:40:03 +0000

    questions controllerにindexアクショんを定義。そしてquestions/index.html.erbを作成

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,7 @@
 class QuestionsController < ApplicationController
   
   def index
-    @questions = Question.all
-    
+    @questions = Question.where(chapter_number: params[:chapter_number])
   end
   
   def new
