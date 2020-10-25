f8d405a719b2960d84c1c9d2e4650095319cb46b
EC2 Default User <ec2-user@ip-172-31-32-173.ap-northeast-1.compute.internal>
2020-10-25 22:16:10 +0000

    questions_controllerのprivateにparams.requreとpermitに:chapter_numと:user_idを追加

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,7 @@
 class QuestionsController < ApplicationController
   
   def index
+    @question = Question.new
     @questions = Question.where(chapter_number: params[:chapter_number])
     render "index_#{params[:chapter_number]}"
   end
@@ -9,12 +10,12 @@ class QuestionsController < ApplicationController
   end
   
   def create
-    Question.create(chapter_number: 0, text: question_params[:text], user_id: current_user.id)
+    Question.create(chapter_number: params[:chapter_number], text: question_params[:text], user_id: current_user.id)
   end
   
   private
   def question_params
-    params.permit(:text)
+    params.require(:question).permit(:chapter_number, :text, :user_id)
   end
   
 end
